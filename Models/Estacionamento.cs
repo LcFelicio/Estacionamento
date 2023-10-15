0636ff533571234c3540f39c8ceb351b3442d74f
gr-Soares <gustavo.rg.soares@gmail.com>
2023-10-14 22:46:04 -0300

    Models Estacionamento e Funcionario

---
+++ Models/Estacionamento.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Estacionamento.Models
+{
+
+    public enum EstadoEnum { AC, AL, AP, AM, BA, CE, DF, ES, GO, MA, MT, MS, MG, PA, PB, PR, PE, PI, RJ, RN, RS, RO, RR, SC, SP, SE, TO }
+
+    [Table("estacionamento")]
+    public class Estacionamento
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "ID: ")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo Nome é Obrigatório")]
+        [Display(Name = "Nome: ")]
+        public string Nome { get; set; }
+
+        [Display(Name = "Endereço: ")]
+        public string Endereco { get; set; }
+
+        [Display(Name = "Cidade: ")]
+        public String Cidade { get; set; }
+
+        [Display(Name = "Estado: ")]
+        public EstadoEnum Estado { get; set; }
+
+        [Display(Name = "Quantidade Vagas P: ")]
+        public int QtdeVagasP { get; set; }
+
+        [Display(Name = "Quantidade Vagas M: ")]
+        public int QtdeVagasM { get; set; }
+
+        [Display(Name = "Quantidade Vagas G: ")]
+        public int QtdeVagasG { get; set; }
+
+        [Required(ErrorMessage = "Campo Tarifa é Obrigatório")]
+        [Display(Name = "Tarifa: ")]
+        public double Tarifa { get; set; }
+
+        [Required(ErrorMessage = "Campo Valor p/Hora é Obrigatório")]
+        [Display(Name = "Valor p/Hora: ")]
+        public double ValorHora { get; set; }
+
+    }
+}
---
+++ Models/Funcionario.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Estacionamento.Models
+{
+
+    public enum Funcao { MANOBRISTA, SEGURANCA, PORTEIRO, ADMINISTRADOR }
+
+    public class Funcionario
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "ID: ")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo Nome é Obrigatório")]
+        [Display(Name = "Nome: ")]
+        public string Nome { get; set; }
+
+        [Display(Name = "Endereço: ")]
+        public string Endereco { get; set; }
+
+        [Display(Name = "Cidade: ")]
+        public String Cidade { get; set; }
+
+        [Display(Name = "Estado: ")]
+        public EstadoEnum Estado { get; set; }
+
+        [Required(ErrorMessage = "Campo CPF é Obrigatório")]
+        [Display(Name = "CPF: ")]
+        public String Cpf { get; set; }
+
+        [Required(ErrorMessage = "Campo Função é Obrigatório")]
+        [Display(Name = "Função: ")]
+        public Funcao Funcao { get; set; }
+    }
+}
